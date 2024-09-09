fb098d319c106bf5f76557f272bf4c1a81696f24
Saiful Kurniawan <saiful.ka96@gmail.com>
2024-09-09 11:25:23 +0700

    Update pdo_connection.php
    simplify the code

--- pdo_connection.php
+++ pdo_connection.php
@@ -1,10 +1,7 @@
 <?php
-
-// ini adalah class untuk mengambil koneksi PDO berdasarkan parameter 
-// saat ini hanya tersedia untuk koneksi mysql dan sqlserver
-// 1. atur connection_string dulu sebelum digunakan, line 10-13 untuk sqlserver, line 16-19 untuk mysql
-// 2. panggil class ini dari function anda dengan parameter sql_type = mysql / sqlsrv
-
+// ini adalah class untuk mengambil koneksi PDO. saat ini hanya tersedia untuk koneksi mysql dan sqlserver
+// 1. atur connection_string dulu sebelum digunakan, line 7-10 untuk sqlserver, line 13-16 untuk mysql
+// 2. panggil class ini dari function anda dengan sql_typeeter sql_type = mysql / sqlsrv
 
 // SQL SERVER CONNECTION STRING
 define('SSSERVER', "yourservername, port");
@@ -18,67 +15,24 @@ define('MSDBNAME', "yourdbname");
 define('MSDBUSER', "yourdbuser");
 define('MSDBPASS', "yourdbpass");
 
-
-class pdo_connection 
-{
-    private function connection_string ($sql_type)
-    {
-        switch ($sql_type)
-        {
-            case "sqlsrv":
-                return [
-                        "server"    => SSSERVER,
-                        "dbname"    => SSDBNAME,
-                        "dbuser"    => SSDBUSER,
-                        "dbpass"    => SSDBPASS
-                    ];
-                    break;
-            case "mysql":
-                return [
-                        "server"    => MSSERVER,
-                        "dbname"    => MSDBNAME,
-                        "dbuser"    => MSDBUSER,
-                        "dbpass"    => MSDBPASS
-                    ];
-            break;
-            default:
-            return "please define sql type";
-        }
-    }
-            
-    public function handler ($param){
-        if ($param === "mysql") {return $this->pdo_mysql($param);}
-        else if($param === "sqlsrv") {return $this->pdo_sqlserver($param);} 
-        else { return "please provide sql type. sqlsrv / mysql.";}}
+class koneksi {
     
-    protected function pdo_sqlserver ($param)
-    {
-        $con_str    = $this->connection_string($param);
-        try{
-        $pdo        = new PDO("sqlsrv:server=".$con_str['server'].";Database=".$con_str['dbname']."",
-                        "".$con_str['dbuser']."",
-                        "".$con_str['dbpass'].""
-                    );
+    public function handler ($sql_type) { try { # ---> $sql_type = mysql / sqlsrv 
+        if ($sql_type === "mysql") {return $this->pdo_mysql($sql_type);}
+        else if($sql_type === "sqlsrv") {return $this->pdo_sqlserver($sql_type);} 
+        else { throw new Exception("please provide sql type. options: sqlsrv / mysql."); }}
+        catch (Exception $e){ return $e->getMessage(); }}
+    
+    private function pdo_sqlserver () {
+        try{ $pdo   = new PDO("sqlsrv:server=".SSSERVER.";Database=".SSDBNAME."", "".SSDBUSER."", "".SSDBPASS."" );
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) { return $e->getMessage();}
-        return $pdo;
-    } 
+        } catch (PDOException $e) { return $e->getMessage(); }
+        return $pdo; } 
 
-    protected function pdo_mysql ($param)
-    {
-        $con_str    = $this->connection_string($param);
-        try{
-        $pdo        = new PDO("mysql:host=".$con_str['server'].";dbname=".$con_str['dbname']."",
-                        "".$con_str['dbuser']."",
-                        "".$con_str['dbpass'].""
-                    );
+    private function pdo_mysql () {
+        try{ $pdo   = new PDO("mysql:host=".MSSERVER.";dbname=".MSDBNAME."", "".MSDBUSER."", "".MSDBPASS."" );
         $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        } catch (PDOException $e) { return $e->getMessage();}
-        return $pdo;
-    } 
-
-
-
+        } catch (PDOException $e) { return $e->getMessage(); }
+        return $pdo; } 
 }
-
 ?>
